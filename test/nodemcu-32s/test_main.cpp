fde0745e90441a07ed363b3ca34e93bb5709c747
Inbaraj1888 <54296499+Inbaraj1888@users.noreply.github.com>
2019-10-31 17:13:09 +0900

    Update test_main.cpp

--- test/nodemcu-32s/test_main.cpp
+++ test/nodemcu-32s/test_main.cpp
@@ -9,20 +9,20 @@
 // // clean stuff up here
 // }
 
-int ledPin = 2;
+//int ledPin = 2;
 
 void test_led_builtin_pin_number(void) {
-    TEST_ASSERT_EQUAL(13, ledPin);
+    TEST_ASSERT_EQUAL(13, LED_BUILTIN);
 }
 
 void test_led_state_high(void) {
-    digitalWrite(ledPin, HIGH);
-    TEST_ASSERT_EQUAL(HIGH, digitalRead(ledPin));
+    digitalWrite(LED_BUILTIN, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(LED_BUILTIN));
 }
 
 void test_led_state_low(void) {
-    digitalWrite(ledPin, LOW);
-    TEST_ASSERT_EQUAL(LOW, digitalRead(ledPin));
+    digitalWrite(LED_BUILTIN, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(LED_BUILTIN));
 }
 
 void setup() {
